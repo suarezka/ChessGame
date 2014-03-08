e4b3f0ee163c312b3337ba6e6de7eab590dbc76d
DaiLynnDietz <dietzd@mail.gvsu.edu>
2014-03-08 17:56:05 -0500

    Changed and added isValidMove logic

--- ChessGame/src/project3/Rook.java
+++ ChessGame/src/project3/Rook.java
@@ -1,12 +1,5 @@
 package project3;
 
-/************
- * Class that controls the Rook chess piece.
- * 
- * @author Dailynn Dietz and Kaye Suarez
- * @version 2/18/14
- ************/
-
 import gvprojects.chess.model.IChessPiece;
 import gvprojects.chess.model.Move;
 import gvprojects.chess.model.Player;
@@ -14,10 +7,11 @@ import gvprojects.chess.model.Player;
 /************************************************************
  * CIS 163-07
  * Chess Project
+ * Class that controls the rook chess piece behavior
  *
  * @author DaiLynn Dietz
  * @author Kaye Suarez
- * @version Feb 26, 2014
+ * @version Mar 8, 2014
  ************************************************************/
 public class Rook extends ChessPiece {
 
@@ -39,7 +33,7 @@ public class Rook extends ChessPiece {
 	public String type() {
 		return "Rook";
 	}
-	
+
 	/************************************************************
 	 * Determines if a rook can make the desired move
 	 * 
@@ -47,33 +41,69 @@ public class Rook extends ChessPiece {
 	 * @return  True if is valid move
 	 ************************************************************/
 	public boolean isValidMove(Move move, IChessPiece[][] board){
-			
-			//Getting move data for Rook piece
-			int fromC = move.fromColumn;
-			int fromR = move.fromRow;
-			int toC = move.toColumn;
-			int toR = move.toRow;
-			
-			//Parent isValidMove method
-			super.isValidMove(move, board);
-			
-			//Check if move is either front/back or side to side
-			if (fromC != toC || fromR != toR) {
-				return false;
-			}
-			
-			//Check if other pieces in path going right
-			if (fromR == toR && fromC < toC) {
-				//Loop through path to see if piece is in the way
-				while (fromC <= toC) {
-					if (board[fromC][fromR] != null) {
-						return false;
-					}
-					fromC++;
-				}
-			}
-			
-			
-			return true;
+
+		//Getting move data for Rook piece
+		int fromC = move.fromColumn;
+		int fromR = move.fromRow;
+		int toC = move.toColumn;
+		int toR = move.toRow;
+
+		//Parent isValidMove method
+		if(!super.isValidMove(move, board)){
+			return false;
 		}
+
+
+		//Check if move is either front/back or side to side
+		if (fromC != toC || fromR != toR) {
+			return false;
+		}
+
+		int start;
+		int end;
+
+		//Check if other pieces in path going horizontal
+		if (fromR == toR) {
+
+			//Makes it so left/right doesnt matter
+			//The plus one prevents checks at the starting location
+			if(fromC > toC){
+				start = toC + 1;
+				end = fromC;
+			}else{
+				start = fromC + 1;
+				end = toC;
+			}
+
+			//Loop through path to see if piece is in the way
+			while (start < end) {
+				if (board[start][fromR] != null) {
+					return false;
+				}
+				start++;
+			}
+
+			//Checks if other pieces in path going vertical
+		}else{
+
+			//Makes it so left/right doesnt matter
+			if(fromR > toR){
+				start = toR + 1;
+				end = fromR;
+			}else{
+				start = fromR + 1;
+				end = toR;
+			}
+
+			//Loops through path to see if piece is in way
+			while(start < end){
+				if(board[fromC][start] != null){
+					return false;
+				}
+				start++;
+			}
+		}
+
+		return true;
+	}
 }
