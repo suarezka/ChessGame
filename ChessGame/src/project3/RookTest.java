fea8a7f16b55ede3d4aa1f7a9958aef3749bd279
DaiLynnDietz <dietzd@mail.gvsu.edu>
2014-03-08 18:28:22 -0500

    Added More Rook Tests

--- ChessGame/src/project3/RookTest.java
+++ ChessGame/src/project3/RookTest.java
@@ -44,5 +44,116 @@ public class RookTest extends ChessPieceTest {
       board[4][4] = piece;
       assertTrue("Rook Test 1", piece.isValidMove(new Move(4, 4, 4, 1), board));
    }
+   
+   @Test
+   public void canMoveUp() throws Exception {
+	   board[5][5] = piece;
+	   assertTrue("Rook Test 1", piece.isValidMove(new Move(5,5,2,5), board));
+   }
 
+   @Test
+   public void canMoveDown() throws Exception {
+	   board[1][1] = piece;
+	   assertTrue("Rook Test 1", piece.isValidMove(new Move(1,1,5,1),  board));
+   }
+   
+   @Test
+   public void isRook() throws Exception {
+	   assertEquals("Rook Test 1", piece.type().equals("Rook"));
+   }
+   
+   @Test
+   public void checkInvalidMoves() throws Exception {
+	   final int startRow = 3;
+	   final int startCol = 3;
+	   
+	   board[startRow][startCol] = piece;
+	   
+	   //Looping through entire board and trying to move piece there
+	   for(int row = 0; row < board.length; row++){
+		   for(int col = 0; col < board[0].length; col++){
+			   
+			   boolean canMove = piece.isValidMove(
+					   new Move(startRow, startCol, row, col), board);
+			   
+			   //Ensures piece can move in straight line
+			   //Uses exclusive or operator (^) to make sure piece isnt 
+			   //tested at start location
+			   if(row == startRow ^ col == startCol){
+				   assertTrue(canMove);
+				   
+			    //Ensures piece cant move other directions
+			   }else{
+				   assertFalse(canMove);
+			   }
+		   }
+	   }
+   }
+   
+   @Test
+   public void pieceInWayRight() throws Exception {
+	   IChessPiece otherPiece = make(Player.WHITE);
+	   
+	   board[0][0] = piece;
+	   board[0][3] = otherPiece;
+	   
+	   assertFalse("Rook pieceInTheWay Test 1", piece.isValidMove(
+			   new Move(0, 0, 0, 4), board));
+   }
+   
+   @Test
+   public void pieceInWayLeft() throws Exception {
+	   IChessPiece otherPiece = make(Player.WHITE);
+	   
+	   board[6][4] = piece;
+	   board[6][2] = otherPiece;
+	   
+	   assertFalse("Rook pieceInTheWay Test 2", piece.isValidMove(
+			   new Move(6, 4, 6, 1), board));
+   }
+   
+   @Test
+   public void pieceInWayUp() throws Exception {
+	   IChessPiece otherPiece = make(Player.WHITE);
+	   
+	   board[5][2] = piece;
+	   board[4][2] = otherPiece;
+	   
+	   assertFalse("Rook pieceInTheWay Test 3", piece.isValidMove(
+			   new Move(5, 2, 3, 2), board));
+   }
+   
+   @Test
+   public void pieceInWayDown() throws Exception {
+	   IChessPiece otherPiece = make(Player.WHITE);
+	   
+	   board[2][3] = piece;
+	   board[4][3] = otherPiece;
+	   
+	   assertFalse("Rook pieceInTheWay Test 4", piece.isValidMove(
+			   new Move(2, 3, 4, 3), board));
+   }
+   
+   @Test
+   public void pieceOutOfWayHorizontal() throws Exception {
+	   IChessPiece otherPiece = make(Player.WHITE);
+	   
+	   board[3][3] = piece;
+	   board[3][5] = otherPiece;
+	   
+	   assertTrue("Rook pieceOutOfWay Test 1", piece.isValidMove(
+			   new Move(3, 3, 3, 4), board));
+   }
+   
+   @Test
+   public void pieceOutOfWayVertical() throws Exception {
+	   IChessPiece otherPiece = make(Player.WHITE);
+	   
+	   board[2][2] = piece;
+	   board[5][2] = otherPiece;
+	   
+	   assertTrue("Rook pieceOutOfWay Test 2", piece.isValidMove(
+			   new Move(2, 2, 4, 2), board));
+   }
+   
 }
