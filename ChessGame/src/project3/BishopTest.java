121034ee1b5502122d1c8f63f434f3f886dd5e82
DaiLynnDietz <dietzd@mail.gvsu.edu>
2014-03-13 01:31:12 -0400

    More Bishop Tests, Good to go really

--- ChessGame/src/project3/BishopTest.java
+++ ChessGame/src/project3/BishopTest.java
@@ -111,4 +111,49 @@ public class BishopTest extends ChessPieceTest {
 		Move lUp = new Move(7, 7, 0, 0);
 		assertTrue("Bishop Long L-Up Test", piece.isValidMove(lUp, board));
 	}
+	
+	@Test
+	public void doesntMoveThrough() throws Exception {
+		board[0][0] = piece;
+		IChessPiece otherPiece = new Bishop(Player.WHITE);
+		board[1][1] = otherPiece;
+		Move bad = new Move(0, 0, 3, 3);
+		assertFalse("Bishop Move Through Test", 
+				piece.isValidMove(bad, board));
+	}
+	
+	@Test
+	public void doesntJumpOtherPlayer() throws Exception {
+		board[5][1] = piece;
+		IChessPiece otherPiece = new Bishop(Player.BLACK);
+		board[3][3] = otherPiece;
+		Move bad = new Move(5,1,1,5);
+		assertFalse("Bishop Jump Other Player Test",
+				piece.isValidMove(bad, board));
+	}
+	
+	@Test
+	public void doesntMoveNotDiagonal() throws Exception {
+		int fromR = 3;
+		int fromC = 3;
+		
+		board[fromR][fromC] = piece;
+		
+		for(int k = 0; k < board.length; k++){
+			for(int m = 0; m < board[0].length; m++){
+				int rowDiff = Math.abs(fromR - k);
+				int colDiff = Math.abs(fromC - m);
+				
+				Move tempMove = new Move(fromR, fromC, k, m);
+				
+				if(colDiff == rowDiff && fromR != k && fromC != m){
+					assertTrue("Loop bishop true Test",
+							piece.isValidMove(tempMove, board));
+				}else{
+					assertFalse("Loop bishop false Test",
+							piece.isValidMove(tempMove, board));
+				}
+			}
+		}
+	}
 }
