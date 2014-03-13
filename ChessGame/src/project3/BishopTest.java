ce3e850cfecf65b2f6c391bcde5dd8e88853fe53
DaiLynnDietz <dietzd@mail.gvsu.edu>
2014-03-13 01:13:45 -0400

    Added Test Cases, All Cases Work

--- ChessGame/src/project3/BishopTest.java
+++ ChessGame/src/project3/BishopTest.java
@@ -44,16 +44,71 @@ public class BishopTest extends ChessPieceTest {
 	}
 	
 	@Test
-	   public void canMoveInRDownDiagonal() throws Exception {
+	   public void canMoveInRDownDiagonalShort() throws Exception {
 	      board[1][1] = piece;
 	      Move bishopDRight = new Move(1,1,3,3);
-	      assertTrue("Bishop RightDown Diagonal Test", piece.isValidMove(bishopDRight, board));
+	      assertTrue("Bishop RightDown Diagonal Test",
+	    		  piece.isValidMove(bishopDRight, board));
 	   }
 	
 	@Test
-	   public void canMoveInRUpDiagonal() throws Exception {
+	   public void canMoveInRUpDiagonalShort() throws Exception {
 	      board[3][3] = piece;
 	      Move bishopUpRight = new Move(3,3,1,5);
-	      assertTrue("Bishop RightUp Diagonal Test", piece.isValidMove(bishopUpRight, board));
+	      assertTrue("Bishop RightUp Diagonal Test",
+	    		  piece.isValidMove(bishopUpRight, board));
 	   }
+	
+	@Test
+	public void cantMoveStraight() throws Exception {
+		board[3][3] = piece;
+		Move straight = new Move(3, 3, 3, 5);
+		assertFalse("Bishop Straight Move Test",
+				piece.isValidMove(straight, board));
+	}
+	
+	@Test
+	public void canMoveInLeftDownShort() throws Exception {
+		board[5][5] = piece;
+		Move lDown = new Move(5, 5, 6, 6);
+		assertTrue("Bishop Short L-Down Test",
+				piece.isValidMove(lDown, board));
+	}
+	
+	@Test
+	public void canMoveInLeftUpShort() throws Exception {
+		board[5][5] = piece;
+		Move lUp = new Move(5, 5, 4, 4);
+		assertTrue("Bishop Short L-Up Test", piece.isValidMove(lUp, board));
+	}
+	
+	@Test
+	   public void canMoveInRDownDiagonalLong() throws Exception {
+	      board[0][0] = piece;
+	      Move bishopDRight = new Move(0,0,7,7);
+	      assertTrue("Bishop RightDown Diagonal Test Long", 
+	    		  piece.isValidMove(bishopDRight, board));
+	   }
+	
+	@Test
+	   public void canMoveInRUpDiagonalLong() throws Exception {
+	      board[7][0] = piece;
+	      Move bishopUpRight = new Move(7,0,0,7);
+	      assertTrue("Bishop RightUp Diagonal Test Long", 
+	    		  piece.isValidMove(bishopUpRight, board));
+	   }
+	
+	@Test
+	public void canMoveInLeftDownLong() throws Exception {
+		board[0][7] = piece;
+		Move lDown = new Move(0, 7, 7, 0);
+		assertTrue("Bishop Long L-Down Test", piece.isValidMove(lDown, board));
+	}
+	
+	@Test
+	public void canMoveInLeftUpLong() throws Exception {
+		board[7][7] = piece;
+		Move lUp = new Move(7, 7, 0, 0);
+		assertTrue("Bishop Long L-Up Test", piece.isValidMove(lUp, board));
+	}
 }
