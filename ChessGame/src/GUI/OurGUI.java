6f830b047b7d3e4ddd445c218d657c6a8d09410b
DaiLynnDietz <dietzd@mail.gvsu.edu>
2014-03-15 16:11:22 -0400

    Created new GUI

---
+++ ChessGame/src/GUI/OurGUI.java
@@ -0,0 +1,14 @@
+package GUI;
+
+/************************************************************
+ * CIS 163-07
+ * Chess Project
+ * GUI For the chess game
+ *
+ * @author DaiLynn Dietz
+ * @author Kaye Suarez
+ * @version Mar 15, 2014
+ ************************************************************/
+public class OurGUI {
+
+}
