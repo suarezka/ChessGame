622dd1b6f504df04313cad27617915f01b7d6f63
DaiLynnDietz <dietzd@mail.gvsu.edu>
2014-03-18 02:23:07 -0400

    Tried to delete ChessGUI, fatal error. Breaks ourGUI....

--- ChessGame/src/GUI/ChessGUI.java
+++ ChessGame/src/GUI/ChessGUI.java
@@ -17,93 +17,79 @@ import GUI.GVTile.ImageType;
 
 
 public class ChessGUI implements ActionListener {
-    private JFrame top;
-//    private GVTile[][] chessBoard;
-    private JButton[][] chessBoard;
-    private int firstRow, firstCol, secondRow, secondCol;
-    private JLabel status;
-    //private HansButton doit;
-    
-    private ImageIcon bBish = loadIcon("images\\b_bish.png");
-    
-    public ChessGUI()
-    {
-        firstRow = firstCol = -1;
-        top = new JFrame();
-        JPanel pan = new JPanel();
-        
-        pan.setLayout(new GridLayout(8, 8));
-//        chessBoard = new GVTile[8][8]; TODO YOU MIGHT WANT THIS
-        chessBoard = new JButton[8][8];
-        for (int k = 0; k < chessBoard.length; k++)
-            for (int m = 0; m < chessBoard[k].length; m++)
-            {
-//                chessBoard[k][m] = new GVTile((k + m) % 2 == 0); TODO GVTILE STUFF
-            	chessBoard[k][m] = new JButton();
-                chessBoard[k][m].addActionListener(this);
-                //chessBoard[k][m].setBackground(Color.yellow);
-//                chessBoard[k][m].showImage(ImageType.BISHOP, true);
-                chessBoard[k][m].setIcon(bBish);
-                //if (k == m)
-                    //chessBoard[k][m].showImage (GVTile.ImageType.BISHOP, true);
-                pan.add(chessBoard[k][m]);
-            }
-        
-        top.add(pan, BorderLayout.CENTER);
-        status = new JLabel(" Hi ");
-        top.add (status, BorderLayout.SOUTH);
-        top.pack();
-        top.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        top.setVisible(true);
-    }
-    
-    public static void main(String[] args) {
+	private JFrame top;
+	private JButton[][] chessBoard;
+	private int firstRow, firstCol, secondRow, secondCol;
+	private JLabel status;
+
+	private ImageIcon bBish = loadIcon("images\\b_bish.png");
+
+	public ChessGUI()
+	{
+		firstRow = firstCol = -1;
+		top = new JFrame();
+		JPanel pan = new JPanel();
+
+		pan.setLayout(new GridLayout(8, 8));
+		chessBoard = new JButton[8][8];
+		for (int k = 0; k < chessBoard.length; k++)
+			for (int m = 0; m < chessBoard[k].length; m++)
+			{
+				chessBoard[k][m] = new JButton();
+				chessBoard[k][m].addActionListener(this);
+				chessBoard[k][m].setIcon(bBish);
+				pan.add(chessBoard[k][m]);
+			}
+
+		top.add(pan, BorderLayout.CENTER);
+		status = new JLabel(" Hi ");
+		top.add (status, BorderLayout.SOUTH);
+		top.pack();
+		top.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		top.setVisible(true);
+	}
+
+	public static void main(String[] args) {
 		new ChessGUI();
 	}
 
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        Object which = ae.getSource();
-        for (int k = 0; k < chessBoard.length; k++)
-        {
-            for (int m = 0; m < chessBoard[k].length; m++)
-            {
-                if (which == chessBoard[k][m]) {
-                    if (firstCol == -1) {
-                        firstRow = k;
-                        firstCol = m;
-                        status.setText(String.format ("Move from (%d,%d) to?",
-                                firstRow, firstCol));
-                    }
-                    else {
-                        secondRow = k;
-                        secondCol = m;
-                        status.setText(String.format ("(%d,%d) ==> (%d,%d)",
-                                firstRow, firstCol, secondRow, secondCol));
-//                        BufferedImage img = chessBoard[firstRow][firstCol].getImage();
-//                        chessBoard[firstRow][firstCol].setImage(null);
-//                        chessBoard[secondRow][secondCol].setImage(img);
-//                        firstRow = firstCol = -1;
-                    }
-                }
-            }
-        }
-    }
-    
-    /****************************************************************
-     * Static method to load the ImageIcon from the given location.
-     *
-     * TODO you might not need this
-     *
-     * @param name Name of the file.
-     * @return the requested image.
-     ***************************************************************/
-    public static ImageIcon loadIcon(String name) {
-    	java.net.URL imgURL = ChessGUI.class.getResource(name);
-    	if (imgURL == null) {
-    		throw new RuntimeException("Icon resource not found.");
-    	}
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		Object which = ae.getSource();
+		for (int k = 0; k < chessBoard.length; k++)
+		{
+			for (int m = 0; m < chessBoard[k].length; m++)
+			{
+				if (which == chessBoard[k][m]) {
+					if (firstCol == -1) {
+						firstRow = k;
+						firstCol = m;
+						status.setText(String.format ("Move from (%d,%d) to?",
+								firstRow, firstCol));
+					}
+					else {
+						secondRow = k;
+						secondCol = m;
+						status.setText(String.format ("(%d,%d) ==> (%d,%d)",
+								firstRow, firstCol, secondRow, secondCol));
+					}
+				}
+			}
+		}
+	}
 
-    	return new ImageIcon(imgURL);
-    }
+	/****************************************************************
+	 * Static method to load the ImageIcon from the given location.
+	 *
+	 * @param name Name of the file.
+	 * @return the requested image.
+	 ***************************************************************/
+	public static ImageIcon loadIcon(String name) {
+		java.net.URL imgURL = ChessGUI.class.getResource(name);
+		if (imgURL == null) {
+			throw new RuntimeException("Icon resource not found.");
+		}
+
+		return new ImageIcon(imgURL);
+	}
 }
